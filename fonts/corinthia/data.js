c99f28da6eb9f3d2fbc2560383df7adf0cf49fa7
Mac <mac@MacBook-Pro-later.local>
2023-06-18 20:31:08 +0300

    2

---
+++ fonts/corinthia/data.js
@@ -0,0 +1,971 @@
+const corinthia = {
+    name: 'corinthia',
+    ['letters']: {
+        А: {
+            l: 2.475,
+            el: 4,
+            step: 200,
+            getLetter(x) {
+                return ``;
+            }
+        },
+        Б: {
+            l: 3.270,
+            el: 4,
+            step: 165,
+            getLetter(x) {
+                return ``;
+            }
+        },
+        В: {
+            l: 3.095,
+            el: 4,
+            step: 156,
+            getLetter(x) {
+                return ``;
+            }
+        },
+        Г: {
+            l: 2.413,
+            el: 2,
+            step: 164,
+            getLetter(x) {
+                return ``;
+            }
+        },
+        Д: {
+            l: 2.937,
+            el: 3,
+            step: 214,
+            getLetter(x) {
+                return ``;
+            }
+        },
+        Е: {
+            l: 2.121,
+            el: 1,
+            step: 182,
+            getLetter(x) {
+                return ``;
+            }
+        },
+        Ё: {
+            l: 2.331,
+            el: 3,
+            step: 182,
+            getLetter(x) {
+                return ``;
+            }
+        },
+        Ж: {
+            l: 4.135,
+            el: 7,
+            step: 270,
+            getLetter(x) {
+                return ``;
+            }
+        },
+        З: {
+            l: 2.779,
+            el: 3,
+            step: 160,
+            getLetter(x) {
+                return ``;
+            }
+        },
+        И: {
+            l: 2.274,
+            el: 2,
+            step: 195,
+            getLetter(x) {
+                return ``;
+            }
+        },
+        Й: {
+            l: 2.615,
+            el: 3,
+            step: 195,
+            getLetter(x) {
+                return ``;
+            }
+        },
+        К: {
+            l: 2.139,
+            el: 3,
+            step: 168,
+            getLetter(x) {
+                return ``;
+            }
+        },
+        Л: {
+            l: 2.173,
+            el: 1,
+            step: 210.65,
+            getLetter(x) {
+                return ``;
+            }
+        },
+        М: {
+            l: 3.009,
+            el: 1,
+            step: 225.65,
+            getLetter(x) {
+                return ``;
+            }
+        },
+        Н: {
+            l: 2.708,
+            el: 4,
+            step: 195,
+            getLetter(x) {
+                return ``;
+            }
+        },
+        О: {
+            l: 2.753,
+            el: 2,
+            step: 220,
+            getLetter(x) {
+                return ``;
+            }
+        },
+        П: {
+            l: 2.488,
+            el: 2,
+            step: 178,
+            getLetter(x) {
+                return ``;
+            }
+        },
+        Р: {
+            l: 1.886,
+            el: 3,
+            step: 150,
+            getLetter(x) {
+                return ``;
+            }
+        },
+        С: {
+            l: 1.954,
+            el: 1,
+            step: 210,
+            getLetter(x) {
+                return ``;
+            }
+        },
+        Т: {
+            l: 3.260,
+            el: 3,
+            step: 210,
+            getLetter(x) {
+                return ``;
+            }
+        },
+        У: {
+            l: 3.728,
+            el: 2,
+            step: 195,
+            getLetter(x) {
+                return ``;
+            }
+        },
+        Ф: {
+            l: 3.057,
+            el: 5,
+            step: 200,
+            getLetter(x) {
+                return ``;
+            }
+        },
+        Х: {
+            l: 1.790,
+            el: 3,
+            step: 200,
+            getLetter(x) {
+                return ``;
+            }
+        },
+        Ц: {
+            l: 2.841,
+            el: 2,
+            step: 200,
+            getLetter(x) {
+                return ``;
+            }
+        },
+        Ч: {
+            l: 2.111,
+            el: 2,
+            step: 180,
+            getLetter(x) {
+                return ``;
+            }
+        },
+        Ш: {
+            l: 3.535,
+            el: 3,
+            step: 240,
+            getLetter(x) {
+                return ``;
+            }
+        },
+        Щ: {
+            l: 4.130,
+            el: 3,
+            step: 260,
+            getLetter(x) {
+                return ``;
+            },
+        },
+        Ъ: {
+            l: 2.749,
+            el: 1,
+            step: 215,
+            getLetter(x) {
+                return `<path class="cls-1" d="M${11.97 + x},50.83h47.54c10.39,0,18.81,8.39,18.81,18.73v162.41c0,10.34,8.42,18.73,18.81,18.73h50.86c26.26,0,47.55-21.2,47.55-47.36v-16.52c0-26.15-21.29-47.36-47.55-47.36h-63.03"/>`;
+            },
+        },
+        Ы: {
+            l: 3.464,
+            el: 2,
+            step: 200,
+            getLetter(x) {
+                return `<path class="cls-1" d="M${6.1 + x},50.83v180.91c0,10.47,8.42,18.96,18.81,18.96h50.86c26.26,0,47.55-21.47,47.55-47.95v-16.73c0-26.48-21.29-47.95-47.55-47.95h-63.03"/>
+              <line class="cls-1" x1="${174.18 + x}" y1="50.83" x2="${174.18 + x}" y2="250.7" />`;
+            },
+        },
+        Ь: {
+            l: 2.462,
+            el: 1,
+            step: 150,
+            getLetter(x) {
+                return `<path class="cls-1" d="M${6.1 + x},50.83v180.91c0,10.47,8.42,18.96,18.81,18.96h50.86c26.26,0,47.55-21.47,47.55-47.95v-16.73c0-26.48-21.29-47.95-47.55-47.95h-63.03"/>`;
+            },
+        },
+        Э: {
+            l: 1.902,
+            el: 2,
+            step: 200,
+            getLetter(x) {
+                return ``;
+            },
+        },
+        Ю: {
+            l: 4.389,
+            el: 4,
+            step: 280,
+            getLetter(x) {
+                return ``;
+            },
+        },
+        Я: {
+            l: 2.514,
+            el: 2,
+            step: 150,
+            getLetter(x) {
+                return;
+            },
+        },
+        а: {
+            l: 0.957,
+            el: 1,
+            step: 165,
+            getLetter(x) {
+                return;
+            },
+        },
+        б: {
+            l: 1.381,
+            el: 2,
+            step: 180,
+            getLetter(x) {
+                return ``;
+            },
+        },
+        в: {
+            l: 1.999,
+            el: 1,
+            step: 160,
+            getLetter(x) {
+                return ``;
+            },
+        },
+        г: {
+            l: 0.775,
+            el: 1,
+            step: 140,
+            getLetter(x) {
+                return ``;
+            },
+        },
+        д: {
+            l: 2.216,
+            el: 1,
+            step: 160,
+            getLetter(x) {
+                return ``;
+            },
+        },
+        е: {
+            l: 0.739,
+            el: 1,
+            step: 155,
+            getLetter(x) {
+                return ``;
+            },
+        },
+        ё: {
+            l: 0.91,
+            el: 3,
+            step: 155,
+            getLetter(x) {
+                return ``;
+            },
+        },
+        ж: {
+            l: 1.771,
+            el: 5,
+            step: 200,
+            getLetter(x) {
+                return ``;
+            },
+        },
+        з: {
+            l: 1.896,
+            el: 3,
+            step: 135,
+            getLetter(x) {
+                return ``;
+            },
+        },
+        и: {
+            l: 0.788,
+            el: 2,
+            step: 160,
+            getLetter(x) {
+                return ``;
+            },
+        },
+        й: {
+            l: 1.043,
+            el: 3,
+            step: 160,
+            getLetter(x) {
+                return ``;
+            },
+        },
+        к: {
+            l: 2.155,
+            el: 3,
+            step: 120,
+            getLetter(x) {
+                return ``;
+            },
+        },
+        л: {
+            l: 0.946,
+            el: 1,
+            step: 165,
+            getLetter(x) {
+                return ``;
+            },
+        },
+        м: {
+            l: 1.394,
+            el: 1,
+            step: 175,
+            getLetter(x) {
+                return ``;
+            },
+        },
+        н: {
+            l: 0.806,
+            el: 3,
+            step: 165,
+            getLetter(x) {
+                return ``;
+            },
+        },
+        о: {
+            l: 1,
+            el: 3,
+            step: 155,
+            getLetter(x) {
+                return ``;
+            },
+        },
+        п: {
+            l: 0.943,
+            el: 2,
+            step: 155,
+            getLetter(x) {
+                return ``;
+            },
+        },
+        р: {
+            l: 1.760,
+            el: 2,
+            step: 175,
+            getLetter(x) {
+                return ``;
+            },
+        },
+        с: {
+            l: .588,
+            el: 1,
+            step: 155,
+            getLetter(x) {
+                return ``;
+            },
+        },
+        т: {
+            l: 1.795,
+            el: 3,
+            step: 240,
+            getLetter(x) {
+                return ``;
+            },
+        },
+        у: {
+            l: 2.09,
+            el: 2,
+            step: 150,
+            getLetter(x) {
+                return ``;
+            },
+        },
+        ф: {
+            l: 2.428,
+            el: 3,
+            step: 180,
+            getLetter(x) {
+                return ``;
+            },
+        },
+        х: {
+            l: 0.648,
+            el: 3,
+            step: 160,
+            getLetter(x) {
+                return ``;
+            },
+        },
+        ц: {
+            l: 1.05,
+            el: 3,
+            step: 180,
+            getLetter(x) {
+                return ``;
+            },
+        },
+        ч: {
+            l: 0.614,
+            el: 1,
+            step: 140,
+            getLetter(x) {
+                return ``;
+            },
+        },
+        ш: {
+            l: 1.207,
+            el: 3,
+            step: 230,
+            getLetter(x) {
+                return ``;
+            },
+        },
+        щ: {
+            l: 1.326,
+            el: 4,
+            step: 240,
+            getLetter(x) {
+                return ``;
+            },
+        },
+        ъ: {
+            l: 1.035,
+            el: 2,
+            step: 170,
+            getLetter(x) {
+                return ``;
+            },
+        },
+        ы: {
+            l: 1.277,
+            el: 2,
+            step: 170,
+            getLetter(x) {
+                return ``;
+            },
+        },
+        ь: {
+            l: 0.841,
+            el: 1,
+            step: 130,
+            getLetter(x) {
+                return ``;
+            },
+        },
+        э: {
+            l: 0.730,
+            el: 2,
+            step: 150,
+            getLetter(x) {
+                return ``;
+            },
+        },
+        ю: {
+            l: 1.295,
+            el: 3,
+            step: 230,
+            getLetter(x) {
+                return ``;
+            },
+        },
+        я: {
+            l: 1.156,
+            el: 2,
+            step: 140,
+            getLetter(x) {
+                return ``;
+            },
+        },
+        "@": {
+            l: 6.148,
+            el: 1
+        },
+        "$": {
+            l: 3.509,
+            el: 5
+        },
+        "#": {
+            l: 3.442,
+            el: 8
+        },
+        "!": {
+            l: 0.818,
+            el: 2
+        },
+        "?": {
+            l: 1.481,
+            el: 2
+        },
+        ",": {
+            l: 0.055,
+            el: 1
+        },
+        ".": {
+            l: 0.055,
+            el: 1
+        },
+        ":": {
+            l: 0.11,
+            el: 2
+        },
+        "_": {
+            l: 0.677,
+            el: 1
+        },
+        "-": {
+            l: 0.331,
+            el: 1
+        },
+        "#": {
+            l: 3.470,
+            el: 8
+        },
+        "№": {
+            l: 5.064,
+            el: 3
+        },
+        "+": {
+            l: 1.112,
+            el: 3
+        },
+        "=": {
+            l: 1.000,
+            el: 2
+        },
+        "%": {
+            l: 4.2024,
+            el: 3
+        },
+        A: {
+            l: 2.475,
+            el: 4,
+            step: 200,
+            getLetter(x) {
+                return ``;
+            }
+        },
+        B: {
+            l: 3.095,
+            el: 4,
+            step: 156,
+            getLetter(x) {
+                return ``;
+            }
+        },
+        C: {
+            l: 1.954,
+            el: 1,
+            step: 210,
+            getLetter(x) {
+                return ``;
+            }
+        },
+        D: {
+            l: 2.937,
+            el: 3,
+            step: 214,
+            getLetter(x) {
+                return ``;
+            }
+        },
+        E: {
+            l: 2.121,
+            el: 1,
+            step: 182,
+            getLetter(x) {
+                return ``;
+            }
+        },
+        F: {
+            l: 2.303,
+            el: 4,
+        },
+        G: {
+            l: 3.423,
+            el: 4,
+        },
+        H: {
+            l: 2.708,
+            el: 4,
+            step: 195,
+            getLetter(x) {
+                return ``;
+            }
+        },
+        I: {
+            l: 1.751,
+            el: 3
+        },
+        J: {
+            l: 2.912,
+            el: 1
+        },
+        K: {
+            l: 2.139,
+            el: 3,
+            step: 168,
+            getLetter(x) {
+                return ``;
+            }
+        },
+        L: {
+            l: 2.831,
+            el: 3,
+            getLetter(x) {
+            }
+        },
+        M: {
+            l: 3.579,
+            el: 1,
+            step: 225.65,
+            getLetter(x) {
+                return ``;
+            }
+        },
+        N: {
+            l: 2.644,
+            el: 2,
+        },
+        O: {
+            l: 2.753,
+            el: 2,
+            step: 220,
+            getLetter(x) {
+                return ``;
+            }
+        },
+        P: {
+            l: 1.886,
+            el: 3,
+            step: 150,
+            getLetter(x) {
+                return ``;
+            }
+        },
+        Q: {
+            l: 3.085,
+            el: 4,
+        },
+        R: {
+            l: 2.514,
+            el: 3,
+        },
+        S: {
+            l: 2.165,
+            el: 1,
+        },
+        T: {
+            l: 1.84,
+            el: 3,
+            step: 210,
+            getLetter(x) {
+                return ``;
+            }
+        },
+        U: {
+            l: 2.274,
+            el: 2,
+            step: 195,
+            getLetter(x) {
+                return ``;
+            }
+        },
+        V: {
+            l: 1.987,
+            el: 1
+        },
+        W: {
+            l: 3.16,
+            el: 2,
+        },
+        X: {
+            l: 1.790,
+            el: 3,
+            step: 200,
+            getLetter(x) {
+                return ``;
+            }
+        },
+        Y: {
+            l: 3.728,
+            el: 2,
+            step: 195,
+            getLetter(x) {
+                return ``;
+            }
+        },
+        Z: {
+            l: 2.779,
+            el: 3,
+            step: 160,
+            getLetter(x) {
+                return ``;
+            }
+        },
+        a: {
+            l: 0.957,
+            el: 1,
+            step: 165,
+            getLetter(x) {
+                return;
+            },
+        },
+        b: {
+            l: 1.367,
+            el: 2,
+        },
+        c: {
+            l: .588,
+            el: 1,
+            step: 155,
+            getLetter(x) {
+                return ``;
+            },
+        },
+        d: {
+            l: 1.854,
+            el: 2,
+        },
+        e: {
+            l: 0.739,
+            el: 1,
+            step: 155,
+            getLetter(x) {
+                return ``;
+            },
+        },
+        f: {
+            l: 1.874,
+            el: 1,
+        },
+        g: {
+            l: 2.216,
+            el: 1,
+            step: 160,
+            getLetter(x) {
+                return ``;
+            },
+        },
+        h: {
+            l: 2.065,
+            el: 2
+        },
+        i: {
+            l: 0.529,
+            el: 2,
+        },
+        j: {
+            l: 1.66,
+            el: 3,
+        },
+        k: {
+            l: 2.155,
+            el: 3,
+            step: 120,
+            getLetter(x) {
+                return ``;
+            },
+        },
+        l: {
+            l: 1.529,
+            el: 1
+        },
+        m: {
+            l: 1.795,
+            el: 3,
+            step: 240,
+            getLetter(x) {
+                return ``;
+            },
+        },
+        n: {
+            l: 0.943,
+            el: 2,
+            step: 155,
+            getLetter(x) {
+                return ``;
+            },
+        },
+        o: {
+            l: 1,
+            el: 3,
+            step: 155,
+            getLetter(x) {
+                return ``;
+            },
+        },
+        p: {
+            l: 1.760,
+            el: 2,
+            step: 175,
+            getLetter(x) {
+                return ``;
+            },
+        },
+        q: {
+            l: 1.801,
+            el: 1
+        },
+        r: {
+
+            l: 0.614,
+            el: 1,
+            step: 140,
+            getLetter(x) {
+                return ``;
+            },
+        },
+        s: {
+            l: 0.745,
+            el: 1
+        },
+        t: {
+            l: 0.824,
+            el: 3,
+        },
+        u: {
+            l: 0.788,
+            el: 2,
+            step: 160,
+            getLetter(x) {
+                return ``;
+            },
+        },
+        v: {
+            l: 1,
+            el: 1
+        },
+        w: {
+            l: 1.236,
+            el: 3
+        },
+        x: {
+            l: 0.648,
+            el: 3,
+            step: 160,
+            getLetter(x) {
+                return ``;
+            },
+        },
+        y: {
+            l: 2.09,
+            el: 2,
+            step: 150,
+            getLetter(x) {
+                return ``;
+            },
+        },
+        z: {
+            l: 1.896,
+            el: 3,
+            step: 135,
+            getLetter(x) {
+                return ``;
+            },
+        },
+        "1": {
+            l: 1.193,
+            el: 1
+        },
+        "2": {
+            l: 2.321,
+            el: 1
+        },
+        "3": {
+            l: 2.205,
+            el: 2
+        },
+        "4": {
+            l: 2.513,
+            el: 1
+        },
+        "5": {
+            l: 2.523,
+            el: 1
+        },
+        "6": {
+            l: 2.356,
+            el: 1
+        },
+        "7": {
+            l: 1.673,
+            el: 1
+        },
+        "8": {
+            l: 3.142,
+            el: 2
+        },
+        "9": {
+            l: 2.356,
+            el: 1
+        },
+        "0": {
+            l: 2.523,
+            el: 1
+        },
+        " ": {
+            l: 0,
+            el: 0,
+            step: 110,
+            getLetter(x) {
+                return x;
+            },
+        },
+    },
+};
+
+export default corinthia
